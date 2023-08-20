9a5abe9ca7db02aa19e17bf7eb05709f20833ae4
Harold <sadcah@yahoo.com>
2023-08-20 17:05:23 +0100

    feat:started homepage

---
+++ src/components/hompage/index.tsx
@@ -0,0 +1,16 @@
+
+import '../../App.css'
+import './index.css'
+
+function Homepage() {
+
+  return (
+    <div className='container'>
+      <h1>Trivia Trek</h1>
+      <p>Welcome to TriviaTrek, your journey through knowledge and fun! Embark on an exciting quest to test your wits and challenge your intellect. Dive into a world of captivating questions from various categories and explore the realms of trivia. Whether you're a seasoned quizmaster or a curious explorer, TriviaTrek offers an adventure that will entertain and enlighten. Get ready to embark on a captivating journey where learning meets excitement. Begin your TriviaTrek today!</p>
+      <button>Start</button>
+    </div>
+  )
+}
+
+export default Homepage
